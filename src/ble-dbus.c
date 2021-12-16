8588fb732ff839d99e17f89f6d249d0393fb2096
Mans Rullgard <mans@mansr.com>
2021-12-16 15:07:42 +0000

    increase sensor timeout

--- src/ble-dbus.c
+++ src/ble-dbus.c
@@ -316,7 +316,7 @@ static void ble_dbus_expire(void)
 		veItemLocalValue(dev, &val);
 		veVariantToN32(&val);
 
-		if (tick - val.value.UN32 > 60 * TICKS_PER_SEC) {
+		if (tick - val.value.UN32 > 120 * TICKS_PER_SEC) {
 			struct VeDbus *dbus = veItemDbus(dev);
 
 			if (dbus)

102fb698109778dd83e9d143b64db739024535c8
Mans Rullgard <mans@mansr.com>
2022-06-22 14:14:56 +0100

    move company IDs to common header

--- src/ble-scan.h
+++ src/ble-scan.h
@@ -1,6 +1,8 @@
 #ifndef BLE_SCAN_H
 #define BLE_SCAN_H
 
+#define MFG_ID_RUUVI	0x0499
+
 int ble_scan_open(void);
 void ble_scan_continuous(int cont);
 void ble_scan(void);
--- src/ruuvi.h
+++ src/ruuvi.h
@@ -4,8 +4,6 @@
 #include <stdint.h>
 #include <bluetooth/bluetooth.h>
 
-#define MFG_ID_RUUVI 0x0499
-
 int ruuvi_handle_mfg(const bdaddr_t *addr, const uint8_t *buf, int len);
 
 #endif

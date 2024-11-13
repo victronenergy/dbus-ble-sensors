b310ad3341c821d09238a4403a3845a42d41b344
Dirk-Jan Faber <dfaber@victronenergy.com>
2024-11-13 15:19:41 +0100

    Add support for the SolarSense irradiance sensor
    Add support for the bluetooth SolarSense irradiance sensor.
    Also update submodule velib to a version that includes veUnitIrradiance1Dec support

--- src/ble-scan.h
+++ src/ble-scan.h
@@ -4,6 +4,7 @@
 #define MFG_ID_NORDIC	0x0059
 #define MFG_ID_RUUVI	0x0499
 #define MFG_ID_SAFIERY	0x0067
+#define MFG_ID_SOLARSENSE	0x02E1
 
 int ble_scan_open(void);
 void ble_scan_continuous(int cont);

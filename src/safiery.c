1798288f5bbf256b648d9f348cae13533d9a62d6
Mans Rullgard <mans@mansr.com>
2025-03-19 13:31:48 +0000

    safiery: switch to tank class

--- src/safiery.c
+++ src/safiery.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
 
 #include <velib/utils/ve_item_utils.h>
 #include <velib/vecan/products.h>
@@ -10,87 +7,9 @@
 
 #include "ble-dbus.h"
 #include "safiery.h"
-#include "task.h"
+#include "tank.h"
 
 #define HW_ID_TOPDOWN_BLE		10
-
-#define FLUID_TYPE_FRESH_WATER		1
-#define FLUID_TYPE_WASTE_WATER		2
-#define FLUID_TYPE_LIVE_WELL		3
-#define FLUID_TYPE_OIL			4
-#define FLUID_TYPE_BLACK_WATER		5
-#define FLUID_TYPE_GASOLINE		6
-#define FLUID_TYPE_DIESEL		7
-#define FLUID_TYPE_LPG			8
-#define FLUID_TYPE_LNG			9
-#define FLUID_TYPE_HYDRAULIC_OIL	10
-#define FLUID_TYPE_RAW_WATER		11
-
-static struct VeSettingProperties capacity_props = {
-	.type			= VE_FLOAT,
-	.def.value.Float	= 0.2,
-	.min.value.Float	= 0,
-	.max.value.Float	= 1000,
-};
-
-static struct VeSettingProperties fluid_type_props = {
-	.type			= VE_SN32,
-	.def.value.SN32		= 0,
-	.min.value.SN32		= 0,
-	.max.value.SN32		= INT32_MAX - 3,
-};
-
-static struct VeSettingProperties empty_props = {
-	.type			= VE_FLOAT,
-	.def.value.Float	= 0,
-	.min.value.Float	= 0,
-	.max.value.Float	= 500,
-};
-
-static struct VeSettingProperties full_props = {
-	.type			= VE_FLOAT,
-	.def.value.Float	= 20,
-	.min.value.Float	= 0,
-	.max.value.Float	= 500,
-};
-
-static const struct dev_setting safiery_settings[] = {
-	{
-		.name	= "Capacity",
-		.props	= &capacity_props,
-	},
-	{
-		.name	= "FluidType",
-		.props	= &fluid_type_props,
-	},
-};
-
-static const struct dev_setting safiery_topdown_settings[] = {
-	{
-		.name	= "RawValueEmpty",
-		.props	= &full_props,
-	},
-	{
-		.name	= "RawValueFull",
-		.props	= &empty_props,
-	},
-};
-
-static int safiery_init(struct VeItem *root, const void *data)
-{
-	VeVariant v;
-
-	ble_dbus_set_str(root, "RawUnit", "cm");
-	ble_dbus_set_item(root, "Remaining",
-			  veVariantInvalidType(&v, VE_FLOAT), &veUnitm3);
-	ble_dbus_set_item(root, "Level",
-			  veVariantInvalidType(&v, VE_FLOAT), &veUnitNone);
-
-	ble_dbus_add_settings(root, safiery_topdown_settings,
-			      array_size(safiery_topdown_settings));
-
-	return 0;
-}
 
 static const struct reg_info safiery_adv[] = {
 	{
@@ -156,65 +75,18 @@ static const struct reg_info safiery_adv[] = {
 	},
 };
 
+static const struct tank_info safiery_tank_info = {
+	.flags		= TANK_FLAG_TOPDOWN,
+};
+
 static const struct dev_info safiery_sensor = {
+	.dev_class	= &tank_class,
 	.product_id	= VE_PROD_ID_SAFIERY_TANK_SENSOR,
 	.dev_instance	= 20,
 	.dev_prefix	= "safiery_",
-	.role		= "tank",
-	.num_settings	= array_size(safiery_settings),
-	.settings	= safiery_settings,
 	.num_regs	= array_size(safiery_adv),
 	.regs		= safiery_adv,
-	.init		= safiery_init,
 };
-
-static void safiery_update_level(struct VeItem *root)
-{
-	struct VeItem *item;
-	float capacity;
-	int height;
-	int empty;
-	int full;
-	float level;
-	float remain;
-	VeVariant v;
-
-	item = veItemByUid(root, "Capacity");
-	if (!item)
-		return;
-
-	veItemLocalValue(item, &v);
-	veVariantToFloat(&v);
-	capacity = v.value.Float;
-
-	height = veItemValueInt(root, "RawValue");
-	empty = veItemValueInt(root, "RawValueEmpty");
-	full = veItemValueInt(root, "RawValueFull");
-
-	if (empty <= full)
-		goto out_inval;
-
-	level = (float)(height - empty) / (full - empty);
-
-	if (level < 0)
-		level = 0;
-	if (level > 1)
-		level = 1;
-
-	remain = level * capacity;
-
-	ble_dbus_set_int(root, "Level", lrintf(100 * level));
-
-	item = veItemByUid(root, "Remaining");
-	veItemOwnerSet(item, veVariantFloat(&v, remain));
-
-	return;
-
-out_inval:
-	veItemInvalidate(veItemByUid(root, "Level"));
-	veItemInvalidate(veItemByUid(root, "Remaining"));
-	ble_dbus_set_int(root, "Status", 4);
-}
 
 int safiery_handle_mfg(const bdaddr_t *addr, const uint8_t *buf, int len)
 {
@@ -235,7 +107,7 @@ int safiery_handle_mfg(const bdaddr_t *addr, const uint8_t *buf, int len)
 		 addr->b[5], addr->b[4], addr->b[3],
 		 addr->b[2], addr->b[1], addr->b[0]);
 
-	root = ble_dbus_create(dev, &safiery_sensor, NULL);
+	root = ble_dbus_create(dev, &safiery_sensor, &safiery_tank_info);
 	if (!root)
 		return -1;
 
@@ -247,8 +119,6 @@ int safiery_handle_mfg(const bdaddr_t *addr, const uint8_t *buf, int len)
 		return 0;
 
 	ble_dbus_set_regs(root, buf, len);
-
-	safiery_update_level(root);
 	ble_dbus_update(root);
 
 	return 0;

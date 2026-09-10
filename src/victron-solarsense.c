678eecbb4aaba996637c67706e7b571c029a4ef7
Thiemo van Engelen <tvanengelen@victronenergy.com>
2026-09-10 11:31:47 +0200

    solarsense: Remove some items
    These items were meant as debug items and specified as being
    conditional, depending on the value of UnspecifiedRemnant. This was
    however never implemented. So when the use of the corresponding bits
    changes in some future firmware, the values of these items would become
    bogus. Therefore, these items are removed for now until a proper
    solution is implemented.

--- src/victron-solarsense.c
+++ src/victron-solarsense.c
@@ -6,35 +6,6 @@
 #include <velib/types/variant.h>
 #include <velib/utils/ve_item_utils.h>
 
-static int solarsense_xlate_txpower(struct VeItem *root, VeVariant *val,
-				    uint64_t rawval)
-{
-	int txp = rawval ? 6 : 0;
-
-	veVariantUn8(val, txp);
-
-	return 0;
-}
-
-static int solarsense_xlate_tss(struct VeItem *root, VeVariant *val,
-				uint64_t rawval)
-{
-	int tss;
-
-	if (rawval <= 29)
-		tss = rawval * 2;
-	else if (rawval <= 95)
-		tss = 60 + 10 * (rawval - 30);
-	else if (rawval <= 126)
-		tss = 720 + 30 * (rawval - 96);
-	else
-		tss = rawval;
-
-	veVariantUn16(val, tss);
-
-	return 0;
-}
-
 static const struct reg_info solarsense_adv[] = {
 	{
 		.type	= VE_UN32,
@@ -97,55 +68,6 @@ static const struct reg_info solarsense_adv[] = {
 		.name	= "CellTemperature",
 		.format	= &veUnitCelsius1Dec,
 	},
-	{
-		.type	= VE_UN8,
-		.offset	= 105 / 8,
-		.shift	= 105 % 8,
-		.bits	= 1,
-		.name	= "UnspecifiedRemnant",
-		.format	= &veUnitNone,
-	},
-	{
-		.type	= VE_UN16,
-		.offset	= 106 / 8,
-		.shift	= 106 % 8,
-		.bits	= 8,
-		.scale	= 100,
-		.bias	= 1.7,
-		.inval	= 0xff,
-		.flags	= REG_FLAG_INVALID,
-		.name	= "BatteryVoltage",
-		.format	= &veUnitVolt2Dec,
-	},
-	{
-		.type	= VE_UN8,
-		.offset	= 114 / 8,
-		.shift	= 114 % 8,
-		.bits	= 1,
-		.xlate	= solarsense_xlate_txpower,
-		.name	= "TxPowerLevel",
-		.format	= &veUnitdBm,
-	},
-	{
-		.type	= VE_UN16,
-		.offset	= 115 / 8,
-		.shift	= 115 % 8,
-		.bits	= 7,
-		.inval	= 0x7f,
-		.flags	= REG_FLAG_INVALID,
-		.xlate	= solarsense_xlate_tss,
-		.name	= "TimeSinceLastSun",
-		.format	= &veUnitMinutes,
-	},
-};
-
-static const struct alarm solarsense_alarms[] = {
-	{
-		.name	= "LowBattery",
-		.item	= "BatteryVoltage",
-		.level	= 3.2,
-		.hyst	= 0.4,
-	},
 };
 
 static const struct dev_info solarsense_dev_info = {
@@ -153,8 +75,6 @@ static const struct dev_info solarsense_dev_info = {
 	.role		= "meteo",
 	.num_regs	= array_size(solarsense_adv),
 	.regs		= solarsense_adv,
-	.num_alarms	= array_size(solarsense_alarms),
-	.alarms		= solarsense_alarms,
 };
 
 const struct victron_device solarsense_victron_device = {
